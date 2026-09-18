51204dc280a0c521fdee292bc584e8a17c899a0a
trevheg <trevorhegstrom@gmail.com>
2026-09-17 22:46:34 -0700

    commented out stuff the assignment said to delete

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-import {getDb} from "./src/db/connect.js";
+// import {getDb} from "./src/db/connect.js";
 
 // app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
@@ -9,15 +9,15 @@ app.get('/', (req, res) => {
     return res.status(200).json({ message: 'Server is running' });
 })
 
-app.get('/trails', async (req, res) => {
-    try {
-        const trails = await getDb().collection('trails').find({}).toArray();
+// app.get('/trails', async (req, res) => {
+//     try {
+//         const trails = await getDb().collection('trails').find({}).toArray();
 
-        return res.status(200).json(trails);
-    } catch (error) {
-        console.error('Failed to retrieve trails:', error.message);
-        return res.status(500).json({message: 'Failed to retrieve trails'});
-    }
-})
+//         return res.status(200).json(trails);
+//     } catch (error) {
+//         console.error('Failed to retrieve trails:', error.message);
+//         return res.status(500).json({message: 'Failed to retrieve trails'});
+//     }
+// })
 
 export default app;
