f7861ec165342d0f9808a9be6d3a4f49989423cd
trevheg <trevorhegstrom@gmail.com>
2026-09-18 22:14:11 -0700

    Added GET /books route

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 import express from "express";
 const app = express();
+import router from "./src/router.js";
  
 app.use(express.json());
+app.use(router);
 
 app.get('/', (req, res) => {
     return res.status(200).json({ message: 'Server is running' });
 })
-
 
 
 export default app;
