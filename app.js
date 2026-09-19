4ec17a3ddcd1ffc9bc0c26701c0868528c3a1969
trevheg <trevorhegstrom@gmail.com>
2026-09-18 19:18:32 -0700

    got rid of code I commented out

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
 import express from "express";
 const app = express();
-// import {getDb} from "./src/db/connect.js";
-
-// app.use(express.urlencoded({ extended: true}));
+ 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     return res.status(200).json({ message: 'Server is running' });
 })
 
-// app.get('/trails', async (req, res) => {
-//     try {
-//         const trails = await getDb().collection('trails').find({}).toArray();
 
-//         return res.status(200).json(trails);
-//     } catch (error) {
-//         console.error('Failed to retrieve trails:', error.message);
-//         return res.status(500).json({message: 'Failed to retrieve trails'});
-//     }
-// })
 
 export default app;
