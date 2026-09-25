46f4741425224d29e12f1ab62c6e884f28cd57c6
trevheg <trevorhegstrom@gmail.com>
2026-09-25 00:01:33 -0700

    more swagger

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import express from "express";
 const app = express();
 import router from "./src/router.js";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' with { type: 'json' };
  
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(router);
+
 
 app.get('/', (req, res) => {
     return res.status(200).json({ message: 'Server is running' });
