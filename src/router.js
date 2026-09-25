bda0d63d05903cb788626e46dabdd1edcac84d07
trevheg <trevorhegstrom@gmail.com>
2026-09-24 23:55:41 -0700

    added swagger

--- src/router.js
+++ src/router.js
@@ -2,8 +2,79 @@ import express from "express";
 const router = express.Router();
 import { getBooksHandler, getBookByIdHandler } from "./controllers/books.js"
 
+/**
+ * @openapi
+ * /books:
+ *   get:
+ *     tags:
+ *       - Books
+ *     summary: Get all books
+ *     responses:
+ *       200:
+ *         description: A list of books.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 additionalProperties: true
+ *       500:
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Internal server error
+ */
 router.get('/books', getBooksHandler);
 
+/**
+ * @openapi
+ * /books/{id}:
+ *   get:
+ *     tags:
+ *       - Books
+ *     summary: Get a book by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the book to retrieve.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: The requested book.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               additionalProperties: true
+ *       404:
+ *         description: Book not found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Book not found
+ *       500:
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Internal server error
+ */
 router.get('/books/:id', getBookByIdHandler);
 
 export default router;
